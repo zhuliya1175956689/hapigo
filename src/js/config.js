aa477077fe2df5444ed15c7b6d27aea639cbf0d8
zhuliya1175956689 <1175956689@qq.com>
2018-06-25 21:12:41 +0800

    第二次提交

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+require.config({
+	baseUrl : "/",
+	paths : { // 短名称
+		jquery : "lib/jquery/jquery-1.12.4.min",
+		load : "js/loadHeaderAndFooter"
+	}
+});
---
+++ src/js/loadHeaderAndFooter.js
@@ -0,0 +1,5 @@
+/* 加载头部、尾部 */
+define(["jquery"], function($){
+	$(".header").load("/html/include/header.html");
+	$(".footer").load("/html/include/footer.html");
+});
